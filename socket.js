18e611d85fa9e935f2523215032382ead35b95fe
Erbil <erbil.silik@yandex.com>
2018-09-06 11:34:36 +0300

    Create package.json

--- socket.js
+++ socket.js
@@ -1,17 +1,19 @@
-var server = require('http').createServer();
+var server = require("http").createServer();
 
-var io = require('socket.io')(server);
+var io = require("socket.io")(server);
 
-var Redis = require('ioredis');
-var redis = new Redis();
-
-redis.subscribe('test-channel');
-
-redis.on('message', function (channel, message) {
-    console.log(channel, message);
-    // message = JSON.parse(message);
-
-    // io.emit(channel + ':' + message.event, message.data);
+var Redis = require("ioredis");
+var redis = new Redis({
+  host: 'redis'
 });
 
-server.listen(3000);
+redis.subscribe("test-channel");
+
+redis.on("message", function (channel, message) {
+  console.log(channel, message);
+  // message = JSON.parse(message);
+
+  // io.emit(channel + ':' + message.event, message.data);
+});
+
+server.listen(4000);
